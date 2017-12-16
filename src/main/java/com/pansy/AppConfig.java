3c0dda030c6250976dd76191c1f90ffe91cc9614
pansy <526915701@qq.com>
2017-12-16 10:12:24 +0800

    3.10.4 使用过滤器自定义扫描

--- src/main/java/com/pansy/AppConfig.java
+++ src/main/java/com/pansy/AppConfig.java
@@ -2,8 +2,13 @@ package com.pansy;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Repository;
 
 @Configuration
-@ComponentScan(basePackages = "com.pansy")
+@ComponentScan(basePackages = "com.pansy",
+        //3.10.4
+        includeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*Stub.*Repository"),
+        excludeFilters = @ComponentScan.Filter(Repository.class))
 public class AppConfig {
 }
